ea7bfe15435ebe015a673ff2aa63c642ccd0c642
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-01-17 17:09:50 -0300

    Criando interface e usando o NgModel

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero.model';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-  hero = 'Wolverine'
+
+  hero: Hero = {
+    id: 1,
+    name:'Wolverine'
+  }
+
 }
