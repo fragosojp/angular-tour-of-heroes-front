25c657bcba9b48201d02fbf989ef532e7c01c556
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-01-18 09:28:17 -0300

    Lista de Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +12,12 @@ export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name:'Wolverine'
-  }
+  };
 
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
