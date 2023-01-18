7fc3380ce36ce8d933033a1912a13ab956184c42
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-01-18 09:54:51 -0300

    Component Isolado metodo pai controlando filho

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,19 +5,13 @@ import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-
-  hero: Hero = {
-    id: 1,
-    name:'Wolverine'
-  };
-
   heroes = HEROES;
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void{
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
